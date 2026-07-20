8eb681bb34601fd84279da9a099479bdd67df5bd
demac-aruba <gm.demac@gmail.com>
2026-07-20 18:04:51 -0400

    Load responsive inventory layout before screen styles

--- src/screens/InventoryScreen.tsx
+++ src/screens/InventoryScreen.tsx
@@ -1,1 +1,5 @@
-export { InventoryScreen } from './InventoryScreenV4';
+import './inventoryMobileLayoutPatch';
+
+const { InventoryScreen } = require('./InventoryScreenV4') as typeof import('./InventoryScreenV4');
+
+export { InventoryScreen };
