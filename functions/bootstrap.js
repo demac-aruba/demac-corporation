09c4741754055bf81f4bdec263200700b3131673
demac-aruba <gm.demac@gmail.com>
2026-08-06 23:03:25 -0400

    Publish unified WhatsApp Copilot router

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -9,4 +9,5 @@ module.exports = {
   ...require("./professionalReportGeneration"),
   ...require("./whatsappCopilot"),
   ...require("./whatsappCopilotKnowledge"),
+  ...require("./whatsappCopilotRouter"),
 };
