cd21429ef3fef72701a981ee611135a8b2f05bcd
demac-aruba <gm.demac@gmail.com>
2026-08-07 11:53:48 -0400

    Revert accidental direct main write

--- functions/whatsappCopilotRuntimeVersion.js
+++
@@ -1,9 +0,0 @@
-const COPILOT_RUNTIME_VERSION = 17;
-const COPILOT_RUNTIME_SOURCE = "openai+erp-unified-router-v17";
-const COPILOT_FUNCTION_NAME = "whatsappCopilotDraftV17";
-
-module.exports = {
-  COPILOT_FUNCTION_NAME,
-  COPILOT_RUNTIME_SOURCE,
-  COPILOT_RUNTIME_VERSION,
-};
