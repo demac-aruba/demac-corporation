9574c01ba570903732b256307007381b6c8ffe71
demac-aruba <gm.demac@gmail.com>
2026-08-07 12:26:20 -0400

    Export V18 Copilot endpoint explicitly from bootstrap

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -3,13 +3,24 @@ require("./whatsappCopilotPresentation");
 require("./whatsappCopilotCompanyRules");
 require("./whatsappCopilotServiceRules");
 
+const core = require("./index");
+const appointmentNotifications = require("./appointmentNotifications");
+const userManagement = require("./userManagement");
+const voiceTranscription = require("./voiceTranscription");
+const professionalReports = require("./professionalReportGeneration");
+const scheduling = require("./whatsappCopilot");
+const knowledge = require("./whatsappCopilotKnowledge");
+const router = require("./whatsappCopilotRouter");
+
+// Export the public Copilot endpoint explicitly so no earlier spread can shadow
+// the V18 conversation orchestrator with the lower-level scheduling handler.
 module.exports = {
-  ...require("./index"),
-  ...require("./appointmentNotifications"),
-  ...require("./userManagement"),
-  ...require("./voiceTranscription"),
-  ...require("./professionalReportGeneration"),
-  ...require("./whatsappCopilot"),
-  ...require("./whatsappCopilotKnowledge"),
-  ...require("./whatsappCopilotRouter"),
+  ...core,
+  ...appointmentNotifications,
+  ...userManagement,
+  ...voiceTranscription,
+  ...professionalReports,
+  ...scheduling,
+  ...knowledge,
+  whatsappCopilotDraft: router.whatsappCopilotDraft,
 };
