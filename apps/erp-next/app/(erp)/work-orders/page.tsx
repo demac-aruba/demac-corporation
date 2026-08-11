29411e06cc23ce88421c22f75658ee3f4a163919
demac-aruba <gm.demac@gmail.com>
2026-08-11 08:06:12 -0400

    Add explicit material readiness to Work Orders route

--- apps/erp-next/app/(erp)/work-orders/page.tsx
+++ apps/erp-next/app/(erp)/work-orders/page.tsx
@@ -1,6 +1,7 @@
 import { BrowserWorkOrderHandoff } from '../../../components/work-orders/browser-work-order-handoff';
+import { BrowserWorkOrderMaterials } from '../../../components/work-orders/browser-workorder-materials';
 import { WorkOrderCommand } from '../../../components/work-orders/work-order-command';
 
 export default function WorkOrdersPage() {
-  return <><BrowserWorkOrderHandoff /><WorkOrderCommand /></>;
+  return <><BrowserWorkOrderHandoff /><BrowserWorkOrderMaterials /><WorkOrderCommand /></>;
 }
