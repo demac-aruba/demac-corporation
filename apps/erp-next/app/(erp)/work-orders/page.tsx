a74f293d28d7fbe2c2f17e8e36d300881fbe58d4
demac-aruba <gm.demac@gmail.com>
2026-08-11 08:12:11 -0400

    Add consolidated Job Readiness to Work Orders route

--- apps/erp-next/app/(erp)/work-orders/page.tsx
+++ apps/erp-next/app/(erp)/work-orders/page.tsx
@@ -1,7 +1,8 @@
+import { BrowserJobReadiness } from '../../../components/work-orders/browser-job-readiness';
 import { BrowserWorkOrderHandoff } from '../../../components/work-orders/browser-work-order-handoff';
 import { BrowserWorkOrderMaterials } from '../../../components/work-orders/browser-workorder-materials';
 import { WorkOrderCommand } from '../../../components/work-orders/work-order-command';
 
 export default function WorkOrdersPage() {
-  return <><BrowserWorkOrderHandoff /><BrowserWorkOrderMaterials /><WorkOrderCommand /></>;
+  return <><BrowserWorkOrderHandoff /><BrowserWorkOrderMaterials /><BrowserJobReadiness /><WorkOrderCommand /></>;
 }
