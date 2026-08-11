bf67358008109bf5621bc32e76dd2eda504cb2bc
demac-aruba <gm.demac@gmail.com>
2026-08-11 07:36:28 -0400

    Expose governed transfer ledger in Inventory route

--- apps/erp-next/app/(erp)/inventory/layout.tsx
+++ apps/erp-next/app/(erp)/inventory/layout.tsx
@@ -1,5 +1,6 @@
 import { BrowserInventoryReadiness } from '../../../components/inventory/browser-inventory-readiness';
+import { BrowserInventoryTransfers } from '../../../components/inventory/browser-inventory-transfers';
 
 export default function InventoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <><BrowserInventoryReadiness />{children}</>;
+  return <><BrowserInventoryReadiness /><BrowserInventoryTransfers />{children}</>;
 }
