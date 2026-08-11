9f1464015971105709bf119322687a6e9c969ab9
demac-aruba <gm.demac@gmail.com>
2026-08-11 18:09:14 -0400

    Add public search crawler configuration

---
+++ apps/erp-next/app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://demac-aruba.com/sitemap.xml',
+    host: 'https://demac-aruba.com',
+  };
+}
