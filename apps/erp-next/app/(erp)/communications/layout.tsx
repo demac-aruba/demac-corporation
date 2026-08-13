420d10ce9fab4bfb82a32d2436d6ff1f715200e7
demac-aruba <gm.demac@gmail.com>
2026-08-13 16:26:44 -0400

    Keep Communications focused on the team inbox

--- apps/erp-next/app/(erp)/communications/layout.tsx
+++ apps/erp-next/app/(erp)/communications/layout.tsx
@@ -1,5 +1,3 @@
-import { BrowserReportDelivery } from '../../../components/communications/browser-report-delivery';
-
 export default function CommunicationsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <><BrowserReportDelivery />{children}</>;
+  return <>{children}</>;
 }
