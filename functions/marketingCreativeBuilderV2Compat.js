6e47a1fab1b91c767c2abfeaffb7e761177d009d
demac-aruba <gm.demac@gmail.com>
2026-08-17 00:55:43 -0400

    Route production Marketing Creative adapter to V4

--- functions/marketingCreativeBuilderV2Compat.js
+++ functions/marketingCreativeBuilderV2Compat.js
@@ -2,4 +2,4 @@
 // Keep this stable adapter path so existing bootstrap/deployment wiring and the
 // public callable names used by ERP NEXT do not change while the internal
 // creative engine evolves.
-module.exports = require('./marketingCreativeBuilderV3');
+module.exports = require('./marketingCreativeBuilderV4');
