290847389f1b5be3e8cae277d3a4da380b9d9098
demac-aruba <gm.demac@gmail.com>
2026-08-19 12:20:29 -0400

    Add dedicated daily dispatch route

---
+++ apps/erp-next/app/(erp)/scheduling/dispatch/page.tsx
@@ -0,0 +1,5 @@
+import { SchedulingPageShell } from '../../../../components/scheduling/scheduling-page-shell';
+
+export default function DailyDispatchPage() {
+  return <SchedulingPageShell view="dispatch" />;
+}
