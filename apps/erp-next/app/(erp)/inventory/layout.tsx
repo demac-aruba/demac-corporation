a9a6d978660e48572a9b8d6bfcf6418531f27b37
demac-aruba <gm.demac@gmail.com>
2026-08-20 21:55:26 -0400

    Route Inventory through one consolidated workspace

--- apps/erp-next/app/(erp)/inventory/layout.tsx
+++ apps/erp-next/app/(erp)/inventory/layout.tsx
@@ -1,8 +1,3 @@
-import { BrowserInventoryReadiness } from '../../../components/inventory/browser-inventory-readiness';
-import { BrowserInventoryReplenishment } from '../../../components/inventory/browser-inventory-replenishment';
-import { BrowserInventoryTransfers } from '../../../components/inventory/browser-inventory-transfers';
-import { BrowserPurchasingReadiness } from '../../../components/inventory/browser-purchasing-readiness';
-
 export default function InventoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <><BrowserInventoryReadiness /><BrowserInventoryReplenishment /><BrowserPurchasingReadiness /><BrowserInventoryTransfers />{children}</>;
+  return <>{children}</>;
 }
