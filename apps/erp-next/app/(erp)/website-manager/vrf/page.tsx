bc92e277070d5e2ae90a50d31dac8f7c4142c7ec
demac-aruba <gm.demac@gmail.com>
2026-09-11 19:38:53 -0400

    feat(website): add VRF manager route

---
+++ apps/erp-next/app/(erp)/website-manager/vrf/page.tsx
@@ -0,0 +1,5 @@
+import { VrfWebsiteManagerWorkspace } from '@/components/vrf-website-manager-workspace';
+
+export default function VrfWebsiteManagerPage() {
+  return <VrfWebsiteManagerWorkspace />;
+}
