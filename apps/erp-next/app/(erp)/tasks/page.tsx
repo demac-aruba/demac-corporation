e40f8b307da9b2c51e8fd7c9d83e13f07b87f793
demac-aruba <gm.demac@gmail.com>
2026-09-14 20:10:09 -0400

    style(task-tracker): load centered create-task modal styles

--- apps/erp-next/app/(erp)/tasks/page.tsx
+++ apps/erp-next/app/(erp)/tasks/page.tsx
@@ -1,4 +1,5 @@
 import { TaskTrackerWorkspaceV2 } from '@/components/task-tracker/task-tracker-workspace-v2';
+import './task-create-modal.css';
 
 export default function TaskTrackerPage() {
   return <TaskTrackerWorkspaceV2 />;
